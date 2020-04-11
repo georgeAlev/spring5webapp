37cc857457387a20f2d5e3ce0df587e24ab6c81a
jorje <alevou@gmail.com>
2020-04-11 20:12:37 +0300

    creating Author and Book POJOs,
    converting them to JPA entities with ManyTo Many relationship,
    overriding equals(), hashCode() and toString()

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,98 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+import javax.persistence.*;
+
+@Entity
+public class Author{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    /* required by JPA */
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    /* getters */
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    /* setters */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    /* override */
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Author{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", books=").append(books);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    /**
+     * @return  {@code true} if this object is the same as the obj
+     *                       argument OR  have the same id
+     *          {@code false} otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id.equals(author.id);
+    }
+
+    /**
+     * Override to maintain the general contract
+     * that equal objects must have equal hash codes.
+     */
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
